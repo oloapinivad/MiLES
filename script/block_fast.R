f7c0a947e4452f2692e39cf508c89f3e1857d643
Jost von Hardenberg <jost@isac.cnr.it>
2017-05-11 13:08:16 +0200

    eliminated the *_cmd.R scripts

--- script/block_fast.R
+++ script/block_fast.R
@@ -307,3 +307,18 @@ for (var in full_fieldlist)
 nc_close(ncfile2)
 
 }
+
+#read command line
+args <- commandArgs(TRUE)
+if (length(args)!=0) {
+exp=args[1]
+year1=args[2]
+year2=args[3]
+season=args[4]
+ZDIR=args[5]
+BLOCKDIR=args[6]
+PROGDIR=args[7]
+
+source(paste0(PROGDIR,"/script/basis_functions.R"))
+miles.blockfast(exp,year1,year2,season,ZDIR,BLOCKDIR)
+}
--- script/block_figures.R
+++ script/block_figures.R
@@ -165,3 +165,23 @@ for (field in fieldlist) {
 	dev.off()
 	}
 }
+
+#read command line
+args <- commandArgs(TRUE)
+if (length(args)!=0) {
+exp=args[1]
+year1=args[2]
+year2=args[3]
+dataset_ref=args[4]
+year1_ref=args[5]
+year2_ref=args[6]
+season=args[7]
+FIGDIR=args[8]
+EXPDIR=args[9]
+REFDIR=args[10]
+cfg=args[11]
+PROGDIR=args[12]
+
+source(paste0(PROGDIR,"/script/basis_functions.R"))
+miles.blockfigures(exp,year1,year2,dataset_ref,year1_ref,year2_ref,season,FIGDIR,EXPDIR,REFDIR,cfg) 
+}
